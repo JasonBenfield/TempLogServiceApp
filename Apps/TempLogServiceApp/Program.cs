7bad2a0c1dd027e53ac09d877841eb8810081247
Jason Benfield <jbenfield108@gmail.com>
2020-10-14 22:14:14 -0400

    initialized solution

---
+++ Apps/TempLogServiceApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TempLogServiceApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
