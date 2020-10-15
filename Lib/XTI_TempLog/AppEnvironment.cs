6ef722cf09a60277950531e1ec45150a4dd792df
Jason Benfield <jbenfield108@gmail.com>
2020-10-15 14:00:53 -0400

    Start session

---
+++ Lib/XTI_TempLog/AppEnvironment.cs
@@ -0,0 +1,18 @@
+﻿namespace XTI_TempLog
+{
+    public sealed class AppEnvironment
+    {
+        public AppEnvironment(string userName, string requesterKey, string remoteAddress, string userAgent)
+        {
+            UserName = userName;
+            RequesterKey = requesterKey;
+            RemoteAddress = remoteAddress;
+            UserAgent = userAgent;
+        }
+
+        public string UserName { get; }
+        public string RequesterKey { get; }
+        public string RemoteAddress { get; }
+        public string UserAgent { get; }
+    }
+}
---
+++ Lib/XTI_TempLog/TempSessionContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Text.Json;
+using System.Threading.Tasks;
+using XTI_Core;
+
+namespace XTI_TempLog
+{
+    public sealed class TempSessionContext
+    {
+        private readonly TempLog log;
+        private readonly IAppEnvironmentContext appEnvironmentContext;
+        private readonly Clock clock;
+
+        public TempSessionContext(TempLog log, IAppEnvironmentContext appEnvironmentContext, Clock clock)
+        {
+            this.log = log;
+            this.appEnvironmentContext = appEnvironmentContext;
+            this.clock = clock;
+        }
+
+        private string sessionKey;
+
+        public Task StartSession()
+        {
+            var environment = appEnvironmentContext.Value();
+            sessionKey = Guid.NewGuid().ToString("N");
+            var session = new StartSessionModel
+            {
+                SessionKey = sessionKey,
+                TimeStarted = clock.Now(),
+                UserName = environment.UserName,
+                UserAgent = environment.UserAgent,
+                RemoteAddress = environment.RemoteAddress,
+                RequesterKey = environment.RequesterKey
+            };
+            var serialized = JsonSerializer.Serialize(session);
+            log.Write($"session.{sessionKey}.log", serialized);
+            return Task.CompletedTask;
+        }
+    }
+}
